c844b4c27111cd7bc941a553a018a0039569a36a
Arthur Gabriel da silva nascimento <152107936+mizu-a@users.noreply.github.com>
2025-02-06 14:38:16 -0400

    Add files via upload

---
+++ filadeimpressão.py
@@ -0,0 +1,58 @@
+  class  FilaDeImpressao:
+    
+
+  def configurar_inicial(self):
+    self.fia=[]
+
+    def adicionar_trabalho():
+    self.fila.append(trabalho)
+    print (f"Trabalho  '{Dtrabalho}'  adicionado a fila de impressão")
+
+  def processar_trabalho(self):
+    if not self_vazia():
+      trabalho = self.fila.pop(0)
+      print(f"o trabalkho '{trabalho}' esta sendo processado")
+
+      else:
+        print("a fila esta vazia")
+
+        def Mostrar_fila(self):
+          if self.esta.vazia():
+            print("a fila essta vazia")
+            else:
+              print("fila atual de impressão:")
+              for trabalho in self.fila:
+                print (f"{trabalho}")
+
+    def esta_vazia(self):
+      return len(self.fila == 0)           
+
+  def nenu ():
+    fila_impressao= FilaDeImpressao()
+    fila_impressao.configurar_inicial()
+
+
+
+  while true:
+  print ("Opções")
+  print ("1.adicionar um trabalho na fila de impressão")
+  print ("2.Imprimir o proximo trabalho da fila.")
+  print ("3. Mostrar a fila de impressão")
+  print ("4. SairS")
+  escolha = input("escolha uma opção de 1-4".)
+
+  if escolha == "1"
+    trabalho = input("digite o nome do trabalho a ser impresso")
+    fila_impressao.adicionar_trabalho(trabalho)
+    
+    elif escolha == "2":
+      fila_impressao.processar_trabalho()
+
+    elif escolha == "3":
+      fila_impressao.Mostrar_fila()
+
+    elif escolha == "4":
+      print("saindo do programa")
+      break
+      else:
+          print("Opção invalida.")
